97ad908b5b701f191592ebb4267fe9c228743003
termi17385 <66066987+termi17385@users.noreply.github.com>
2020-11-18 00:42:14 +1100

    GamePlayUpdate

---
+++ Assets/Scripts/Rotators.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Rotators : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+    [SerializeField]
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            // flips player depending on the direction of the arrow
+            player.isRight = !sprite.flipX;
+            player.isLeft = sprite.flipX;
+
+            //if (sprite.flipX == false)
+            //{
+            //    player.isRight = true;
+            //    player.isLeft = false;
+            //}
+            //else if(sprite )
+             
+        }
+    }
+}
